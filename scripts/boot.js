11d3998e0490d7375378bc833f88a72c5c47e88d
Kenzie Campbell <kenzie@route19.com>
2013-09-19 10:16:09 -0300

    Initial commit

---
+++ scripts/boot.js
@@ -0,0 +1,34 @@
+// how long to show the splash screen
+var stage_delay = 5 * 1000;
+// initial url for display
+var stage_url   = "http://live.almacinnissportscentre.com/";
+
+
+$(document).ready(function(){
+  delayed_load_stage();
+})
+
+
+// delay the load
+function delayed_load_stage(){
+  setTimeout(function(){load_stage();},stage_delay);
+}
+
+// load the remote stage content
+function load_stage(){
+  $.ajax({
+    url: stage_url,
+    cache: false
+  }).done(function( html ) {
+    transition_stage(html);
+  }).fail(function(){
+    delayed_load_stage();
+  });
+}
+
+// transition from splash screen to stage content
+function transition_stage(html){
+  $('#pre_stage').fadeOut('slow', function(){
+    $("#stage").html(html).fadeIn();
+  });
+}
